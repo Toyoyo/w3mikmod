b6b589d8f491f677feff83f36d28fe97c5222638
Toyoyo Toyo <toyoyo@wirefull.org>
2021-01-30 17:05:59 +0100

    0.1.1, volume bars, Samples/Instruments static resizing

--- w3mikmod.c
+++ w3mikmod.c
@@ -26,21 +26,25 @@
 #include <winuser.h>
 #include <mikmod.h>
 
-#define W3MIDMOKVERSION "0.1.0"
+#define W3MIDMOKVERSION "0.1.1"
 #define MOD_EXT "*.669;*.amf;*.dsm;*.far;*.gdm;*.it;*.imf;*.mod;*.med;*.mtm;*.okt;*.s3m;*.stm;*.stx;*.ult;*.umx;*.xm"
 #define NOMOD "-- No module loaded --"
+#define MAXVOICES 256
 #define MikMod_Sleep(ns) Sleep(ns / 1000)
 
 /* function definitions */
 LRESULT CALLBACK fnWndProcMain(HWND hWnd, unsigned int msg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK fnWndProcSamples(HWND hWnd, unsigned int msg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK fnWndProcInstr(HWND hWnd, unsigned int msg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK fnWndProcVol(HWND hWnd, unsigned int msg, WPARAM wParam, LPARAM lParam);
 void CALLBACK Timer0Proc(HWND hWnd, unsigned int msg, unsigned int idTimer, DWORD dwTime);
 void UpdateSamples(void);
 void UpdateInstr(void);
+void UpdateVol(BOOL UpdateChn);
 void UpdateStatus(void);
 void SwitchSamplesBTN();
 void SwitchInstrBTN();
+void SwitchVolBTN();
 void PlayListNext(int PlayListCount, int Start);
 void PlayListPrev(int PlayListCount, int Start);
 void ModuleLoad(char *ModFN, int Play);
@@ -62,12 +66,18 @@ HWND hSamplesStatic;
 HWND hSamplesWin;
 HWND hInstrStatic;
 HWND hInstrWin;
+HWND hVolStatic;
+HWND hVolWin;
 HWND hPList;
 HWND hToolbar;
 HBRUSH hBrushMain;
 HBRUSH hBrushSamples;
 HBRUSH hBrushInstr;
+HBRUSH hBrushVol;
 HBRUSH hBrushNfo;
+HBRUSH hBrushGreen;
+HBRUSH hBrushYellow;
+HBRUSH hBrushRed;
 HFONT s_hFont;
 int TimeCount = 0;
 int FontX, FontY;
@@ -77,6 +87,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
   WNDCLASS wcMain;
   WNDCLASS wcSamples;
   WNDCLASS wcInstr;
+  WNDCLASS wcVol;
   MSG messages;
   HWND hwnd;
   char CfgFont[1024];
@@ -86,7 +97,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
   hBrushMain = CreateSolidBrush(RGB(0,0,0));
   hBrushSamples = CreateSolidBrush(RGB(0,0,0));
   hBrushInstr = CreateSolidBrush(RGB(0,0,0));
+  hBrushVol = CreateSolidBrush(RGB(0,0,0));
   hBrushNfo = CreateSolidBrush(RGB(0,0,205));
+
+  hBrushGreen = CreateSolidBrush(RGB(0,205,0));
+  hBrushYellow = CreateSolidBrush(RGB(205,205,0));
+  hBrushRed = CreateSolidBrush(RGB(205,0,0));
 
   CoInitialize(NULL);
   InitCommonControls();
@@ -120,6 +136,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
   wcInstr.cbWndExtra    = sizeof(void*);
   wcInstr.hIcon = (HICON)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(4), IMAGE_ICON, 32, 32, 0);
   RegisterClass(&wcInstr);
+
+  /* volume window */
+  memset(&wcVol,0,sizeof(WNDCLASS));
+  wcVol.lpszClassName = "w3mikmod_vol";
+  wcVol.lpfnWndProc   = fnWndProcVol;
+  wcVol.hbrBackground = hBrushVol;
+  wcVol.hInstance     = hInstance;
+  wcVol.cbWndExtra    = sizeof(void*);
+  wcVol.hIcon = (HICON)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(5), IMAGE_ICON, 32, 32, 0);
+  RegisterClass(&wcVol);
 
   md_mode |= DMODE_SOFT_MUSIC | DMODE_INTERP | DMODE_SURROUND | DMODE_HQMIXER | DMODE_16BITS | DMODE_SOFT_SNDFX;
 
@@ -166,11 +192,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
   DeleteObject(hBrushMain);
   DeleteObject(hBrushSamples);
   DeleteObject(hBrushInstr);
+  DeleteObject(hBrushVol);
   DeleteObject(hBrushNfo);
+  DeleteObject(hBrushGreen);
+  DeleteObject(hBrushYellow);
+  DeleteObject(hBrushRed);
+
   DeleteObject(s_hFont);
   UnregisterClass("w3mikmod_main", hInstance);
   UnregisterClass("w3mikmod_samples", hInstance);
   UnregisterClass("w3mikmod_instr", hInstance);
+  UnregisterClass("w3mikmod_vol", hInstance);
   Player_Stop();
   Sleep(1000);
   MikMod_free(module);
@@ -187,7 +219,7 @@ LRESULT CALLBACK fnWndProcMain(HWND hWnd, unsigned int msg, WPARAM wParam, LPARA
   HDC hdcMain;
   TEXTMETRIC tm;
   TBADDBITMAP tbab;
-  TBBUTTON tbb[14];
+  TBBUTTON tbb[11];
 
   switch(msg)
   {
@@ -217,83 +249,78 @@ LRESULT CALLBACK fnWndProcMain(HWND hWnd, unsigned int msg, WPARAM wParam, LPARA
       SendMessage(hToolbar, TB_ADDBITMAP, 9, (LPARAM)&tbab);
       SendMessage(hToolbar, TB_ADDBITMAP, 10, (LPARAM)&tbab);
       SendMessage(hToolbar, TB_ADDBITMAP, 11, (LPARAM)&tbab);
+      SendMessage(hToolbar, TB_ADDBITMAP, 12, (LPARAM)&tbab);
+      SendMessage(hToolbar, TB_ADDBITMAP, 13, (LPARAM)&tbab);
 
       /* Toolbar definition */
       ZeroMemory(tbb, sizeof(tbb));
-      tbb[0].fsStyle = TBSTYLE_SEP;
-      tbb[0].iBitmap = 1;
+      tbb[0].iBitmap = 0;
+      tbb[0].fsState = TBSTATE_ENABLED;
+      tbb[0].fsStyle = TBSTYLE_BUTTON;
+      tbb[0].idCommand = 2000;
 
-      tbb[1].iBitmap = 0;
+      tbb[1].iBitmap = 1;
       tbb[1].fsState = TBSTATE_ENABLED;
       tbb[1].fsStyle = TBSTYLE_BUTTON;
-      tbb[1].idCommand = 2000;
+      tbb[1].idCommand = 2001;
 
-      tbb[2].fsStyle = TBSTYLE_SEP;
-      tbb[2].iBitmap = 5;
+      tbb[2].iBitmap = 2;
+      tbb[2].fsState = TBSTATE_ENABLED;
+      tbb[2].fsStyle = TBSTYLE_BUTTON;
+      tbb[2].idCommand = 2002;
 
-      tbb[3].iBitmap = 1;
+      tbb[3].iBitmap = 3;
       tbb[3].fsState = TBSTATE_ENABLED;
       tbb[3].fsStyle = TBSTYLE_BUTTON;
-      tbb[3].idCommand = 2001;
+      tbb[3].idCommand = 2003;
 
-      tbb[4].iBitmap = 2;
+      tbb[4].iBitmap = 4;
       tbb[4].fsState = TBSTATE_ENABLED;
       tbb[4].fsStyle = TBSTYLE_BUTTON;
-      tbb[4].idCommand = 2002;
+      tbb[4].idCommand = 2004;
 
-      tbb[5].iBitmap = 3;
+      tbb[5].iBitmap = 5;
       tbb[5].fsState = TBSTATE_ENABLED;
       tbb[5].fsStyle = TBSTYLE_BUTTON;
-      tbb[5].idCommand = 2003;
+      tbb[5].idCommand = 2005;
 
-      tbb[6].fsStyle = TBSTYLE_SEP;
-      tbb[6].iBitmap = 5;
+      tbb[6].iBitmap = 6;
+      tbb[6].fsState = TBSTATE_ENABLED;
+      tbb[6].fsStyle = TBSTYLE_BUTTON;
+      tbb[6].idCommand = 2009;
 
-      tbb[7].iBitmap = 4;
+      tbb[7].iBitmap = 7;
       tbb[7].fsState = TBSTATE_ENABLED;
       tbb[7].fsStyle = TBSTYLE_BUTTON;
-      tbb[7].idCommand = 2004;
+      tbb[7].idCommand = 2006;
 
-      tbb[8].iBitmap = 5;
+      tbb[8].iBitmap = 8;
       tbb[8].fsState = TBSTATE_ENABLED;
       tbb[8].fsStyle = TBSTYLE_BUTTON;
-      tbb[8].idCommand = 2005;
+      tbb[8].idCommand = 2007;
 
-      tbb[9].fsStyle = TBSTYLE_SEP;
-      tbb[9].iBitmap = 5;
+      tbb[9].iBitmap = 9;
+      tbb[9].fsState = TBSTATE_ENABLED;
+      tbb[9].fsStyle = TBSTYLE_BUTTON;
+      tbb[9].idCommand = 2008;
 
+      tbb[10].fsStyle = TBSTYLE_SEP;
       tbb[10].iBitmap = 6;
-      tbb[10].fsState = TBSTATE_ENABLED;
-      tbb[10].fsStyle = TBSTYLE_BUTTON;
-      tbb[10].idCommand = 2006;
 
-      tbb[11].iBitmap = 7;
-      tbb[11].fsState = TBSTATE_ENABLED;
-      tbb[11].fsStyle = TBSTYLE_BUTTON;
-      tbb[11].idCommand = 2007;
-
-      tbb[12].fsStyle = TBSTYLE_SEP;
-      tbb[12].iBitmap = 6;
-
-      tbb[13].iBitmap = 8;
-      tbb[13].fsState = TBSTATE_ENABLED;
-      tbb[13].fsStyle = TBSTYLE_BUTTON;
-      tbb[13].idCommand = 2008;
-
-      ClientResize(hWnd, 320, 161);
+      ClientResize(hWnd, 331, 161);
       SendMessage(hToolbar, TB_ADDBUTTONS, sizeof(tbb)/sizeof(TBBUTTON), (LPARAM)&tbb);
       SendMessage(hToolbar, TB_AUTOSIZE, 0, 0);
 
       /* Our controls */
-      hFileName = CreateWindowEx(0,"Static","-- FileName --", WS_CHILD|WS_VISIBLE, 0, 38, 320, 14, hWnd,(HMENU)3002,hIns,0);
-      hSongName = CreateWindowEx(0,"Static","-- SongName --", WS_CHILD|WS_VISIBLE, 0, 52, 320, 14, hWnd,(HMENU)3001,hIns,0);
-      hTrackerType = CreateWindowEx(0,"Static","-- Tracker Name --", WS_CHILD|WS_VISIBLE, 0, 66, 320, 14, hWnd,(HMENU)3002,hIns,0);
-      CreateWindowEx(0,"Static","", WS_CHILD|WS_VISIBLE|SS_WHITERECT, 298, 97, 22, 100, hWnd,(HMENU)3004,hIns,0);
-      hStatus = CreateWindowEx(0,"Static","-- Status --", WS_CHILD|WS_VISIBLE, 0, 80, 320, 16, hWnd,(HMENU)3000,hIns,0);
-      hPList = CreateWindowEx(0,"Listbox","",WS_CHILD|WS_VISIBLE|LBS_HASSTRINGS|LBS_EXTENDEDSEL|LBS_NOINTEGRALHEIGHT|WS_VSCROLL,0,98,296,62,hWnd,(HMENU)3100,hIns,0);
-      hPListAdd = CreateWindowEx(0,"Button","+",WS_TABSTOP|WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON|BS_VCENTER,299,98,20,20,hWnd,(HMENU)3101,hIns,0);
-      hPListDel = CreateWindowEx(0,"Button","-",WS_TABSTOP|WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON|BS_VCENTER,299,119,20,20,hWnd,(HMENU)3102,hIns,0);
-      hPListLoad = CreateWindowEx(0,"Button",">",WS_TABSTOP|WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON|BS_VCENTER,299,140,20,20,hWnd,(HMENU)3103,hIns,0);
+      hFileName = CreateWindowEx(0,"Static","-- FileName --", WS_CHILD|WS_VISIBLE, 0, 38, 331, 14, hWnd,(HMENU)3002,hIns,0);
+      hSongName = CreateWindowEx(0,"Static","-- SongName --", WS_CHILD|WS_VISIBLE, 0, 52, 331, 14, hWnd,(HMENU)3001,hIns,0);
+      hTrackerType = CreateWindowEx(0,"Static","-- Tracker Name --", WS_CHILD|WS_VISIBLE, 0, 66, 331, 14, hWnd,(HMENU)3002,hIns,0);
+      CreateWindowEx(0,"Static","", WS_CHILD|WS_VISIBLE|SS_WHITERECT, 309, 97, 22, 100, hWnd,(HMENU)3004,hIns,0);
+      hStatus = CreateWindowEx(0,"Static","-- Status --", WS_CHILD|WS_VISIBLE, 0, 80, 331, 16, hWnd,(HMENU)3000,hIns,0);
+      hPList = CreateWindowEx(0,"Listbox","",WS_CHILD|WS_VISIBLE|LBS_HASSTRINGS|LBS_EXTENDEDSEL|LBS_NOINTEGRALHEIGHT|WS_VSCROLL,0,98,307,62,hWnd,(HMENU)3100,hIns,0);
+      hPListAdd = CreateWindowEx(0,"Button","+",WS_TABSTOP|WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON|BS_VCENTER,310,98,20,20,hWnd,(HMENU)3101,hIns,0);
+      hPListDel = CreateWindowEx(0,"Button","-",WS_TABSTOP|WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON|BS_VCENTER,310,119,20,20,hWnd,(HMENU)3102,hIns,0);
+      hPListLoad = CreateWindowEx(0,"Button",">",WS_TABSTOP|WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON|BS_VCENTER,310,140,20,20,hWnd,(HMENU)3103,hIns,0);
 
       SendMessage(hStatus, WM_SETFONT, (WPARAM)s_hFont, (LPARAM)MAKELONG(TRUE, 0));
       SendMessage(hSongName, WM_SETFONT, (WPARAM)s_hFont, (LPARAM)MAKELONG(TRUE, 0));
@@ -324,6 +351,11 @@ LRESULT CALLBACK fnWndProcMain(HWND hWnd, unsigned int msg, WPARAM wParam, LPARA
         CW_USEDEFAULT, CW_USEDEFAULT, 320, SamplesInitSize,
         NULL, NULL, hIns, NULL);
       SetWindowLong(hInstrWin,0*sizeof(void*),(long)hBrushInstr);
+
+      hVolWin=CreateWindowEx(0, "w3mikmod_vol", "W3MikMod - Volume", WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+        CW_USEDEFAULT, CW_USEDEFAULT, 320, SamplesInitSize,
+        NULL, NULL, hIns, NULL);
+      SetWindowLong(hVolWin,0*sizeof(void*),(long)hBrushVol);
 
       return 0;
     }
@@ -472,6 +504,12 @@ LRESULT CALLBACK fnWndProcMain(HWND hWnd, unsigned int msg, WPARAM wParam, LPARA
       if(HIWORD(wParam) == BN_CLICKED && LOWORD(wParam) == 2005)
       {
         SwitchInstrBTN();
+      }
+
+      /* volume bars */
+      if(HIWORD(wParam) == BN_CLICKED && LOWORD(wParam) == 2009)
+      {
+        SwitchVolBTN();
       }
 
       /* Playlist prev */
@@ -647,6 +685,42 @@ LRESULT CALLBACK fnWndProcSamples(HWND hWnd, unsigned int msg, WPARAM wParam, LP
   return (DefWindowProc(hWnd, msg, wParam, lParam));
 }
 
+LRESULT CALLBACK fnWndProcVol(HWND hWnd, unsigned int msg, WPARAM wParam, LPARAM lParam)
+{
+  switch(msg)
+  {
+    case WM_CREATE:
+    {
+      HINSTANCE hIns = ((LPCREATESTRUCT)lParam)->hInstance;
+      hVolStatic = CreateWindowEx(0,"Static",NOMOD, WS_CHILD|WS_VISIBLE, 0, 0, 1280, 1024, hWnd,(HMENU)3000,hIns,0);
+      SendMessage(hVolStatic, WM_SETFONT, (WPARAM)s_hFont, (LPARAM)MAKELONG(TRUE, 0));
+      return 0;
+    }
+    case WM_CLOSE:
+    {
+      SwitchVolBTN();
+      return 0;
+    }
+    case WM_CTLCOLORSTATIC:
+    {
+      SetBkMode((HDC)wParam,TRANSPARENT);
+      SetTextColor((HDC)wParam, RGB(255,255,255));
+      return GetWindowLong(hWnd,0*sizeof(void*));
+    }
+    case WM_SIZE:
+    {
+      if(LOWORD(wParam) == 0) {
+        UpdateVol(TRUE);
+      }
+    }
+    case WM_DESTROY:
+    {
+      return 0;
+    }
+  }
+  return (DefWindowProc(hWnd, msg, wParam, lParam));
+}
+
 LRESULT CALLBACK fnWndProcInstr(HWND hWnd, unsigned int msg, WPARAM wParam, LPARAM lParam)
 {
   switch(msg)
@@ -687,6 +761,8 @@ void CALLBACK Timer0Proc(HWND hWnd, unsigned int msg, unsigned int idTimer, DWOR
 {
   int PlayListCount=SendMessage(hPList, LB_GETCOUNT, 0, 0);
 
+  if(Player_Paused() && isStarted) UpdateVol(FALSE);
+
   if(isPlaying) {
     PlayListCount=SendMessage(hPList, LB_GETCOUNT, 0, 0);
     if(Player_Active()) {
@@ -694,6 +770,7 @@ void CALLBACK Timer0Proc(HWND hWnd, unsigned int msg, unsigned int idTimer, DWOR
       MikMod_EnableOutput();
       MikMod_Update();
       UpdateStatus();
+      UpdateVol(FALSE);
       TimeCount++;
     } else {
       if(PlayListCount == 0 || PlayListCount == LB_ERR) {
@@ -759,8 +836,10 @@ void UpdateSamples(void) {
     }
 
     TitleBarSize=GetSystemMetrics(SM_CYSIZEFRAME)+GetSystemMetrics(SM_CYCAPTION)+GetSystemMetrics(SM_CYBORDER);
-    SetWindowPos(hSamplesWin, HWND_BOTTOM, 0,0 , MaxWidth*FontX, TitleBarSize+(Lines*FontY),
+    SetWindowPos(hSamplesWin, HWND_BOTTOM, 0, 0 , MaxWidth*FontX, TitleBarSize+(Lines*FontY),
       SWP_NOMOVE|SWP_NOACTIVATE|SWP_NOZORDER);
+    SetWindowPos(hSamplesStatic, HWND_BOTTOM, 0, 0, MaxWidth*FontX, Lines*FontY, SWP_NOMOVE|SWP_NOACTIVATE|SWP_NOZORDER);
+    RedrawWindow(hSamplesWin, NULL, NULL, RDW_ERASE | RDW_INVALIDATE);
   }
 }
 
@@ -802,8 +881,65 @@ void UpdateInstr(void) {
     }
 
     TitleBarSize=GetSystemMetrics(SM_CYSIZEFRAME)+GetSystemMetrics(SM_CYCAPTION)+GetSystemMetrics(SM_CYBORDER);
-    SetWindowPos(hInstrWin, HWND_BOTTOM, 0,0 , MaxWidth*FontX, TitleBarSize+(Lines*FontY),
+    SetWindowPos(hInstrWin, HWND_BOTTOM, 0, 0 , MaxWidth*FontX, TitleBarSize+(Lines*FontY),
       SWP_NOMOVE|SWP_NOACTIVATE|SWP_NOZORDER);
+    SetWindowPos(hInstrStatic, HWND_BOTTOM, 0, 0, MaxWidth*FontX, Lines*FontY, SWP_NOMOVE|SWP_NOACTIVATE|SWP_NOZORDER);
+    RedrawWindow(hInstrWin, NULL, NULL, RDW_ERASE | RDW_INVALIDATE);
+  }
+}
+
+void UpdateVol(BOOL UpdateChn) {
+  if(module && !IsIconic(hVolWin)) {
+    char VolMsg[1024] = {0};
+    int Lines = 0;
+    int MaxWidth = 0;
+    int CurWidth = 0;
+    int TitleBarSize = 0;
+    int t = 0;
+    int c = 0;
+    int ChanVol = 0;
+    HDC hdc;
+    VOICEINFO vi[MAXVOICES];
+
+    for (t = 0; t < module->numchn; t++) {
+      if (strlen(strchr(&VolMsg, '\0')) < 768)
+        snprintf(strchr(&VolMsg, '\0'), 255, "%02d: \n", t);
+    }
+
+    for(c=0; VolMsg[c]!='\0'; c++) {
+      CurWidth++;
+      if(VolMsg[c] == '\n') {
+        if(CurWidth > MaxWidth) MaxWidth = CurWidth;
+        Lines++;
+        CurWidth=0;
+      }
+    }
+
+    if(UpdateChn) {
+      SetWindowText(hVolStatic, &VolMsg);
+      TitleBarSize=GetSystemMetrics(SM_CYSIZEFRAME)+GetSystemMetrics(SM_CYCAPTION)+GetSystemMetrics(SM_CYBORDER);
+      SetWindowPos(hVolWin, HWND_BOTTOM, 0, 0 , FontX*4+101+GetSystemMetrics(SM_CXSIZEFRAME)+GetSystemMetrics(SM_CXBORDER), TitleBarSize+(Lines*FontY),
+        SWP_NOMOVE|SWP_NOACTIVATE|SWP_NOZORDER);
+      SetWindowPos(hVolStatic, HWND_BOTTOM, 0, 0, FontX*4, 1024, SWP_NOMOVE|SWP_NOACTIVATE|SWP_NOZORDER);
+      RedrawWindow(hVolWin, NULL, NULL, RDW_ERASE | RDW_INVALIDATE);
+    }
+
+    hdc = GetDC(hVolWin);
+    Player_QueryVoices(module->numchn, vi);
+    for (t = 0; t < module->numchn; t++) {
+      if (vi[t].volume > 0) {
+        ChanVol = Voice_RealVolume(Player_GetChannelVoice(t))*100/65535;
+      } else {
+        ChanVol = 0;
+      }
+      SelectObject(hdc, hBrushVol);
+      Rectangle(hdc, FontX*4, FontY*t, FontX*4+100, FontY+FontY*t);
+      SelectObject(hdc, hBrushGreen);
+      if(ChanVol > 50) SelectObject(hdc, hBrushYellow);
+      if(ChanVol > 80) SelectObject(hdc, hBrushRed);
+      Rectangle(hdc, FontX*4, FontY*t, FontX*4+ChanVol, FontY+FontY*t);
+    }
+    ReleaseDC(hVolWin, hdc);
   }
 }
 
@@ -817,6 +953,7 @@ void ModuleLoad(char *ModFN, int Play) {
   if(module) {
     UpdateSamples();
     UpdateInstr();
+    UpdateVol(TRUE);
     module->loop = 0;
 
     isLoaded=1;
@@ -859,16 +996,23 @@ void UpdateStatus(void) {
 
 void SwitchSamplesBTN() {
   int WinVisible=IsWindowVisible(hSamplesWin);
-  SendMessage(hToolbar, TB_CHANGEBITMAP, (WPARAM)2004, (LPARAM)4+5*(1-WinVisible));
+  SendMessage(hToolbar, TB_CHANGEBITMAP, (WPARAM)2004, (LPARAM)4+6*(1-WinVisible));
   SendMessage(hToolbar, TB_CHECKBUTTON, (WPARAM)2004, (LPARAM)1-WinVisible);
   ShowWindow(hSamplesWin, 5*(1-WinVisible));
 }
 
 void SwitchInstrBTN() {
   int WinVisible=IsWindowVisible(hInstrWin);
-  SendMessage(hToolbar, TB_CHANGEBITMAP, (WPARAM)2005, (LPARAM)5+5*(1-WinVisible));
+  SendMessage(hToolbar, TB_CHANGEBITMAP, (WPARAM)2005, (LPARAM)5+6*(1-WinVisible));
   SendMessage(hToolbar, TB_CHECKBUTTON, (WPARAM)2005, (LPARAM)1-WinVisible);
   ShowWindow(hInstrWin, 5*(1-WinVisible));
+}
+
+void SwitchVolBTN() {
+  int WinVisible=IsWindowVisible(hVolWin);
+  SendMessage(hToolbar, TB_CHANGEBITMAP, (WPARAM)2009, (LPARAM)6+6*(1-WinVisible));
+  SendMessage(hToolbar, TB_CHECKBUTTON, (WPARAM)2009, (LPARAM)1-WinVisible);
+  ShowWindow(hVolWin, 5*(1-WinVisible));
 }
 
 char *ReplStr(const char *str, const char *from, const char *to) {
